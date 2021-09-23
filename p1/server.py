a29c4ed9b8f66b5cf86db9fcc66f5231807e454e
cdceballor1 <cdceballor@gmail.com>
2021-09-23 12:24:08 -0500

    adding getNode and returning host and port

--- p1/server.py
+++ p1/server.py
@@ -10,8 +10,10 @@ import os
 import constants
 import shutil
 import sys
+import string
 from _thread import *
 
+minusculas = string.ascii_lowercase   # O si prefieres pones "abcde...z" que es lo mismo
 
 def handleConecction(client):
 	while True:
@@ -19,10 +21,34 @@ def handleConecction(client):
 		options = request.split(',')
 		checkStatus(options)
 
+def letra_a_codigo(letra):
+    return minusculas.index(letra) + 97
+
+def getNode(key): 
+# (negro, azul, marrón, gris, verde, naranja, rosa, púrpura, rojo, blanco y amarillo) 
+# y los 28 adicionales 
+# (turquesa, verde oliva, verde menta, borgoña, lavanda, magenta, salmón, cian, beige, 
+# rosado, verde oscuro, verde oliva, lila, amarillo pálido, fucsia, mostaza, ocre, 
+# trullo, malva, púrpura oscuro, verde lima, verde claro, ciruela, azul claro, melocotón, 
+# violeta, tan y granate.
+# A a F, G a L, M a S, T a Z
+	keyVerification = letra_a_codigo(key[0])
+	if (keyVerification >= 97 and keyVerification <= 102):
+		return host, port
+	elif (keyVerification >= 103 and keyVerification <= 108):
+		return host, port
+	elif (keyVerification >= 109 and keyVerification <= 115):
+		return host, port
+	elif (keyVerification >= 116 and keyVerification <= 122):
+		return host, port
+	else:
+		return "0",0
+    
+    
 
 def checkStatus(options):
 	key = options[0]
-	#host, port = getNode(key)
+	host, port = getNode(key)
 	#TO DO connect to node and send data
 	#sendToNode(host, port)
 
