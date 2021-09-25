5d500b721a67fbaa9c3f95bab95de21bb303a4b2
sloaizac <v-sloaiza@trueaccord.com>
2021-09-25 13:45:47 -0500

    add node 2

--- p1/constants.py
+++ p1/constants.py
@@ -6,9 +6,9 @@ Created by: Sebastian Loaiza Correa
 ADDRESSTOSAVE= "C:/UNIVERSITY/ST0246-TopicosTelematica-CristianCeballos/Lab1/data/archivo.txt"
 SERVER_ADDRESS = "54.173.238.28"
 NODE1_IP = "107.23.128.94"
-NODE2_IP = "107.23.128.94"
+NODE2_IP = "54.91.1.116"
 NODE3_IP = "107.23.128.94"
-NODE4_IP = "107.23.128.94"
+NODE4_IP = "54.91.1.116"
 PORT = 8050
 BACKLOG = 5
 BUFFER_SIZE = 1024
