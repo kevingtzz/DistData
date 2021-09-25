76ce65a97c7d2f95fe376fa20e18525b6b6edf56
kevin Gutierrez <kevingtzz@outlook.com>
2021-09-25 12:10:18 -0500

    test

--- p1/constants.py
+++ p1/constants.py
@@ -4,11 +4,11 @@ Created by: Sebastian Loaiza Correa
 """
 
 ADDRESSTOSAVE= "C:/UNIVERSITY/ST0246-TopicosTelematica-CristianCeballos/Lab1/data/archivo.txt"
-SERVER_ADDRESS = "18.208.197.194"
-NODE1_IP = ""
-NODE2_IP = ""
-NODE3_IP = ""
-NODE4_IP = ""
+SERVER_ADDRESS = "54.173.238.28"
+NODE1_IP = "107.23.128.94"
+NODE2_IP = "107.23.128.94"
+NODE3_IP = "107.23.128.94"
+NODE4_IP = "107.23.128.94"
 PORT = 8050
 BACKLOG = 5
 BUFFER_SIZE = 1024
