5537daee7479b61c3f3c0285ece635247a03409c
sloaizac <v-sloaiza@trueaccord.com>
2021-09-25 12:18:57 -0500

    fixed cristian CAGADA

--- p1/node.py
+++ p1/node.py
@@ -20,26 +20,26 @@ nombre_archivo=constants.ADDRESSTOSAVE
 data = dict()
 
 def handleConecction(client):
-    	while True:
-    		request = client.recv(constants.BUFFER_SIZE).decode('ascii')
-			options = request.split(',')
-			checkStatus(options)
+	while True:
+		request = client.recv(constants.BUFFER_SIZE).decode('ascii')
+		options = request.split(',')
+		checkStatus(options)
 
 
 def checkStatus(options):
-    if options[0] == constants.PUT:
+	if options[0] == constants.PUT:
 		createBucket(options[1])
 	elif options[0] == constants.GET:
-    	get(options[1])
+		get(options[1])
 	elif options[0] == constants.UPDATE:
-    	getFileList(options[1])
+		getFileList(options[1])
 	elif options[0] == constants.DELETE:
-    	deleteFile(options[1], options[2])
+		deleteFile(options[1], options[2])
 
 """
 saveData(nombre_archivo, dataToSave):
 	nombre_archivo -> Name of the file (with the address)
-	dataToSave -> could be the options (is the data that we going to save early) 
+	dataToSave -> could be the options (is the data that we going to save early)
 """
 def saveData(nombre_archivo, dataToSave):
     archivo = open(nombre_archivo, "wb")
@@ -49,7 +49,7 @@ def saveData(nombre_archivo, dataToSave):
 """
 callData(nombre_archivo)
 	nombre_archivo -> Call the file (same address)
-	Use the pickle library to unicode the data saved and return it 
+	Use the pickle library to unicode the data saved and return it
 """
 def callData(nombre_archivo):
     archivo = open(nombre_archivo, "rb")
